0d701b2be3c22aa393a71d882eab85272c0b4bc4
Dan Karaj <hey@dkaraj.com>
2015-02-05 13:17:12 +0000

    nice looking forms yo

--- web/public/js/login.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-       	$("#login_form").append('<form action="/login" method="post">Email:<br><input type="text" name="email">'
-       								+ '<br><br>Password:<br>'
-									+ '<input type="password" name="password">'
-									+ '<br><br><input type="submit" value="Submit"></form>');
-});
--- web/public/js/signup.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-       	$("#signup_form").append('<form action="/signup" method="post">Email:<br><input type="text" name="email">'
-       								+ '<br><br>Password:<br>'
-									+ '<input type="password" name="password">'
-									+ '<br><br><input type="submit" value="Submit"></form>');
-});
