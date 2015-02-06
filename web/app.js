edfac120be15f297746ca43c72550f43b6322e0f
Nathanael <natemail@mail.com>
2015-02-06 15:24:22 +0000

    account creation and logging in fixed@

--- web/app.js
+++ web/app.js
@@ -6,10 +6,13 @@ var mongoose = require('mongoose');
 var passport = require('passport');
 var flash    = require('connect-flash');
 
-var morgan       = require('morgan');
+var logger       = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser   = require('body-parser');
 var session      = require('express-session');
+var exphbs      = require('express3-handlebars');
+
+var engines = require('consolidate');
 
 var configDB = require('./config/database.js');
 
@@ -17,20 +20,25 @@ mongoose.connect(configDB.url); // connect to our database
 
 
 //app.use(express.static(__dirname)); // Current directory is root
-app.use(express.static(path.join(__dirname, 'public'))); //  "public" off of current is root
+app.use(express.static(__dirname + '/public')); //  "public" off of current is root
 
-
-require('./config/passport')(passport); // pass passport for configuration
+require('./config/passport.js')(passport); // pass passport for configuration
 
 // set up our express application
-app.use(morgan('dev')); // log every request to the console
+app.use(logger('dev')); // log every request to the console
 app.use(cookieParser()); // read cookies (needed for auth)
-app.use(bodyParser()); // get information from html forms
+app.use(bodyParser.urlencoded({ extended: true })); // get information from html forms
 
-app.set('view engine', 'ejs'); // set up ejs for templating
+app.engine('html', engines.mustache);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/public');
 
 // required for passport
-app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(session({ 
+					secret: 'voioioioioioioio',
+					resave: false,
+					saveUninitialized: false
+				 }));
 app.use(passport.initialize());
 app.use(passport.session()); // persistent login sessions
 app.use(flash()); // use connect-flash for flash messages stored in session
