6cabff2db067012629190933ed857263fe4cc5fb
Srijan Parmeshwar <srijan.parmeshwar@gmail.com>
2015-02-24 13:47:41 +0000

    Fixing bugs.

--- analysis/SimpleFaceMain.cpp
+++ analysis/SimpleFaceMain.cpp
@@ -85,9 +85,9 @@ int main(int argc, char** argv) {
 		std::vector<Timestamp> timestamps = ls.processVideo(std::string(argv[1]), 3); 
 		std::cout << "Starting gif production with : " << timestamps.size() << std::endl;
 		f.extractGifs(std::string(argv[1]), std::string(argv[2]), 0, timestamps);
-	//	confirm(sp[4].c_str());
-	} catch(std::string s) {
-		std::cout << s << std::endl;
+		//confirm(sp[4].c_str());
+	} catch(const char* s) {
+		std::cout << std::string(s) << std::endl;
 	}
 	return 0;
 }
--- analysis/SimpleFaceStrategy.cpp
+++ analysis/SimpleFaceStrategy.cpp
@@ -156,7 +156,7 @@ std::vector<Timestamp> SimpleFaceStrategy::processVideo(const std::string & file
 
 			double endTime = endFrame / fps;
 			double startTime = startFrame / fps;
-			timestamps.push_back(Timestamp(startTime, endTime));
+			timestamps.push_back(Timestamp(startTime*1000, endTime*1000));
 		}
 		if (added == numGIFs)
 		{
