16375564a9aaaa5c13c07c23c623a01294e66068
Matt Cornford <matt.d.cornford@gmail.com>
2015-02-24 12:42:28 +0000

    Added more output to check for correct operation

--- analysis/SimpleFaceMain.cpp
+++ analysis/SimpleFaceMain.cpp
@@ -13,7 +13,10 @@ int main(int argc, char** argv) {
 	try {
 		Filter f;
 		SimpleFaceStrategy ls;
+
+		std::cout << "Starting analysis" < std::endl;
 		std::vector<Timestamp> timestamps = ls.processVideo(std::string(argv[1]), 3); 
+		std::cout << "Starting gif production with : " << timestamps.size() << std::endl;
 		f.extractGifs(std::string(argv[1]), std::string(argv[2]), 0, timestamps);
 	} catch(std::string s) {
 		std::cout << s << std::endl;
