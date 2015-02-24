f68df57116b1fc71b3bdd597c8d8cf72a934edc0
Matt Cornford <matt.d.cornford@gmail.com>
2015-02-24 12:50:06 +0000

    Fixed error

--- analysis/SimpleFaceMain.cpp
+++ analysis/SimpleFaceMain.cpp
@@ -81,7 +81,7 @@ int main(int argc, char** argv) {
 		Filter f;
 		SimpleFaceStrategy ls;
 
-		std::cout << "Starting analysis" < std::endl;
+		std::cout << "Starting analysis" << std::endl;
 		std::vector<Timestamp> timestamps = ls.processVideo(std::string(argv[1]), 3); 
 		std::cout << "Starting gif production with : " << timestamps.size() << std::endl;
 		f.extractGifs(std::string(argv[1]), std::string(argv[2]), 0, timestamps);
