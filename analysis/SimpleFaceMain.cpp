585e76bbe04fb33b92facf7e06ab006e74846c3f
Matt Cornford <matt.d.cornford@gmail.com>
2015-02-24 13:16:41 +0000

    W/o network code

--- analysis/SimpleFaceMain.cpp
+++ analysis/SimpleFaceMain.cpp
@@ -85,7 +85,7 @@ int main(int argc, char** argv) {
 		std::vector<Timestamp> timestamps = ls.processVideo(std::string(argv[1]), 3); 
 		std::cout << "Starting gif production with : " << timestamps.size() << std::endl;
 		f.extractGifs(std::string(argv[1]), std::string(argv[2]), 0, timestamps);
-		confirm(sp[4].c_str());
+	//	confirm(sp[4].c_str());
 	} catch(std::string s) {
 		std::cout << s << std::endl;
 	}
