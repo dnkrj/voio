455a6a312d420d76b167bc71f1baf2e8fe8a52c9
Matt Cornford <matt.d.cornford@gmail.com>
2015-02-26 14:17:56 +0000

    Added code for email pingback into new file, and its not called from Entry

---
+++ analysis/EmailPingback.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h> /* socket, connect */
+#include <netinet/in.h> /* struct sockaddr_in, struct sockaddr */
+#include <netdb.h> /* struct hostent, gethostbyname */
+
+void printError(const char *msg) { perror(msg); exit(1); }
+
+int confirm(const char *arg)
+{
+    /* first what are we going to send and where are we going to send it? */
+    int portno =        80;
+    char *host =        "localhost";
+    char *message_fmt = "GET /ready?user=%s HTTP/1.0\n";
+
+    struct hostent *server;
+    struct sockaddr_in serv_addr;
+    int sockfd, bytes, sent, total;
+    char message[1024];
+
+    /* fill in the parameters */
+    sprintf(message, message_fmt, arg);
+    printf("Request: %s\n",message);
+
+    /* create the socket */
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) printError("ERROR opening socket");
+
+    /* lookup the ip address */
+    server = gethostbyname(host);
+    if (server == NULL) printError("ERROR, no such host");
+
+    /* fill in the structure */
+    memset(&serv_addr,0,sizeof(serv_addr));
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(portno);
+    memcpy(&serv_addr.sin_addr.s_addr,server->h_addr,server->h_length);
+
+    /* connect the socket */
+    if (connect(sockfd,(struct sockaddr *)&serv_addr,sizeof(serv_addr)) < 0)
+        printError("ERROR connecting");
+
+    /* send the request */
+    total = strlen(message);
+    sent = 0;
+    do {
+        bytes = write(sockfd,message+sent,total-sent);
+        if (bytes < 0)
+            printError("ERROR writing message to socket");
+        if (bytes == 0)
+            break;
+        sent+=bytes;
+    } while (sent < total);
+
+    /* close the socket */
+    close(sockfd);
+
+    return 0;
+}
+
