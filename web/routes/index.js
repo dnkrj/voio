9ad1e5570c513aae8fbcd091510fce4113d36640
Nathanael <natemail@mail.com>
2015-03-03 11:51:34 +0000

    fixed only showing 1 reblogged gif

--- web/routes/index.js
+++ web/routes/index.js
@@ -284,12 +284,18 @@ module.exports = function(passport) {
 	/* GET reblog a gif */
 	router.get('/reblog', isLoggedIn(true), function(req, res) {
 		var gif = req.query.gif;
+		var op  = req.query.op;
+		console.log('reblogging: ' + gif + " " + op);
+		var gifObject = {};
+		gifObject['gif'] = gif;
+		gifObject['op']  = op;
+		console.log(gifObject);
 	    User.update(
 				{ '_id' : req.user._id },
-				{ $push: { "local.reblog_gifs" : gif} },
+				{ $push: { "local.reblog_gifs_ops" : gifObject } },
 				function(err) {
 					if (err) {
-						console.log("/// Failed to reblog gif.")
+						console.log("/// Failed to reblog gif.");
 						console.log(err);
 					}
 					res.end();
@@ -328,6 +334,8 @@ module.exports = function(passport) {
 			var gifOps = [];
 			var stream = User.find( { '_id' : { $in : req.user.local.subscribe } } ).stream();
 			stream.on('data', function(user) {
+				console.log("pause");
+				stream.pause();
 				var i, j;
 				for (i=0; i < user.local.own_gifs.length; i++) {
 					if (DBgifs.indexOf('"' + user.local.own_gifs[i] + '.mp4"') === -1) {
@@ -335,21 +343,22 @@ module.exports = function(passport) {
 						DBgifs.push('"' + user.local.own_gifs[i] + '.mp4"');
 					}
 				}
-				for (j=0; j < user.local.reblog_gifs.length; j++) {
-					stream.pause();
-					Gif.findById( user.local.reblog_gifs[j], function(err, gif) {
-						if (err) {
-							console.log(err);
-						} else if (gif) {
-							if (DBgifs.indexOf('"' + gif._id + '.mp4"') === -1) {
-								gifOps.push('"' + gif.opUsername + '"');
-								DBgifs.push('"' + gif._id + '.mp4"');	
-							}
-						}
-						stream.resume();
-					});
+
+				for (j=0; j < user.local.reblog_gifs_ops.length; j++) {
+					gifObject = user.local.reblog_gifs_ops[j];
+					if (DBgifs.indexOf('"' + gifObject.gif + '.mp4"') === -1) {
+						gifOps.push('"' + gifObject.op + '"');
+						DBgifs.push('"' + gifObject.gif + '.mp4"');	
+					}
+					console.log("ADDING GIF: " + DBgifs);
 				}
-			}).on('close', function() {
+
+
+				console.log("resume");
+				stream.resume();
+			}).on('end', function() {
+				console.log("DBgifs CLOSE: " + DBgifs);
+				console.log("gifOps CLOSE: " + gifOps);
 				res.render('feed', {
 					user : req.user.local,
 			      	title: 'Voio',
